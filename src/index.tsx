fa59717f67682f170f6d2f29a866404c790abb0e
Antonio <antoniofulg@gmail.com>
2022-08-18 00:59:24 -0300

    chore: config tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 
 const container = document.getElementById('main')
 const root = createRoot(container) // createRoot(container!) if you use TypeScript
-root.render(<h1>Olá!</h1>)
+root.render(<h1 className="text-3xl font-bold underline">Olá!</h1>)
